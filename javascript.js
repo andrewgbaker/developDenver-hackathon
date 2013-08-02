a5e9c06990b9f77d2a98d04c5afbdad33cb1a26b
Benton <bentonrochester@gmail.com>
2013-08-02 14:02:27 -0600

    added scripts

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-js
+$(function(){
+	$("#searchForm").submit(function(event) {
+ 
+	  /* stop form from submitting normally */
+	  event.preventDefault();
+	 console.log(event)
+	  /* get some values from elements on the page: */
+	  var $form = $( this ),
+	      term = $form.find( 'input[name="s"]' ).val()
+	      
+	console.log(term)
+
+
+
+	  /* Send the data using post */
+	 // var posting = $.post( url, { s: term } );
+	 
+	  /* Put the results in a div */
+	  // posting.done(function( data ) {
+	  //   var content = $( data ).find( '#content' );
+	  //   $( "#result" ).empty().append( content );
+	  // });
+	});
+
+})
